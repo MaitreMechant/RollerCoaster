f47ac1b5a961d9712fc6efb12c4a18e65643a6bf
ErwannD2D <erwann029@gmail.com>
2016-12-23 00:38:47 +0100

    MapDesign
    DesLumLum

---
+++ RollerCoaster/Assets/Scripts/LumiereOnOff.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LumiereOnOff : MonoBehaviour {
+
+    public Light lumlum;
+
+	// Use this for initialization
+	void Start () {
+
+        lumlum = GetComponent<Light>();
+     
+      
+    }
+
+   
+void Update () {
+ 
+    if (Input.GetKeyDown("space"))
+        {
+        lumlum.intensity = 8f;
+   }
+
+    if (Input.GetKeyUp("space"))
+        {
+       lumlum.intensity = 2f;
+    }
+
+}
+	
+
+
+}
